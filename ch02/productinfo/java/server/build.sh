5d2428dbc08353f318cdca45784be235e9ed9c1d
Finley Ma <fei.ma@kone.com>
2023-11-19 15:04:00 +0800

    update readme level

--- ch02/productinfo/java/server/build.sh
+++ ch02/productinfo/java/server/build.sh
@@ -3,3 +3,5 @@ gradle build
 docker build -f Dockerfile -t registry.cn-shanghai.aliyuncs.com/kone-cndevops/grpc-up-running:ch02 .
 docker push registry.cn-shanghai.aliyuncs.com/kone-cndevops/grpc-up-running:ch02
 ```
+
+# grpcurl grpcserver.dev.247tsa.dps.kone.cn list
